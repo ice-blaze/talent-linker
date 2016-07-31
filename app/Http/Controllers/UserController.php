33d5c825c53e729bff33c5e26c9edd4067f36bf8
Ice-Blaze <franketip@gmail.com>
2016-08-01 01:01:41 +0200

    Simple validation on comments, user and project

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,39 +12,43 @@ use App\Http\Requests;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-      $users = User::all();
-      return view('users.index', compact('users'));
-    }
+  public function index()
+  {
+    $users = User::all();
+    return view('users.index', compact('users'));
+  }
 
-    public function show(User $user)
-    {
-      return view('users.show', compact('user'));
-    }
+  public function show(User $user)
+  {
+    return view('users.show', compact('user'));
+  }
 
-    public function edit(User $user)
-    {
-      $languages = Language::all();
-      $general_skills = GeneralSkill::all();
-      return view('users.edit', compact('user', 'languages', 'general_skills'));
-    }
+  public function edit(User $user)
+  {
+    $languages = Language::all();
+    $general_skills = GeneralSkill::all();
+    return view('users.edit', compact('user', 'languages', 'general_skills'));
+  }
 
-    public function update(User $user)
-    {
-      $user->update(request()->all());
+  public function update(Rquest $request, User $user)
+  {
+    $this->validate($request, [
+      //TODO HAVE AT LEAST ONE LANGUAGE
+    ]);
 
-      // managed the langauges
-      $user->languages()->sync((array)request()->languages);
+    $user->update(request()->all());
 
-      $user->general_skills()->detach();
-      $general_skills = GeneralSkill::find(request()->general_skills);
-      if($general_skills){
-        foreach($general_skills as $skill){
-          $user->general_skills()->save($skill);
-        }
+    // managed the langauges
+    $user->languages()->sync((array)request()->languages);
+
+    $user->general_skills()->detach();
+    $general_skills = GeneralSkill::find(request()->general_skills);
+    if($general_skills){
+      foreach($general_skills as $skill){
+        $user->general_skills()->save($skill);
       }
-
-      return view('users.show', compact('user'));
     }
+
+    return view('users.show', compact('user'));
+  }
 }
