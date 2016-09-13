64024b61249badbfad8b74bb6d5558a980e82202
Ice-Blaze <franketip@gmail.com>
2016-09-13 21:11:37 +0200

    Center the login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,18 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+      <div class="col-md-8 offset-md-4 col-centered">
             <div class="panel panel-default">
-                <div class="panel-heading"><h2>Login</h2></div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} row">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            {{-- <label for="email" class="col-md-4 control-label">E-Mail Address</label> --}}
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+                            <div class="col-md-6 offset-md-6">
+                                <div class="panel-heading"><h2>Login</h2></div>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}"
+                                  placeholder="Email">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -25,10 +26,10 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} row">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            {{-- <label for="password" class="col-md-4 control-label">Password</label> --}}
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
+                            <div class="col-md-6 offset-md-6">
+                                <input id="password" type="password" class="form-control" name="password" placeholder="Password">
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -39,7 +40,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-6 offset-md-6">
                                 <div class="checkbox">
                                     <label>
                                         <input type="checkbox" name="remember"> Remember Me
@@ -49,7 +50,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-6 offset-md-6">
                                 <button type="submit" class="btn btn-primary" name="login">
                                     <i class="fa fa-btn fa-sign-in"></i>Login
                                 </button>
