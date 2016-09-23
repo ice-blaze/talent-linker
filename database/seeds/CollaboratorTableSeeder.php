978c2a797f98dbbdbdaf609a93be56dfca148110
Ice-Blaze <franketip@gmail.com>
2016-09-23 23:50:02 +0200

    Add more cats and projects seed

--- database/seeds/CollaboratorTableSeeder.php
+++ database/seeds/CollaboratorTableSeeder.php
@@ -15,13 +15,24 @@ class CollaboratorTableSeeder extends Seeder
     $user_james = User::find(1);
     $user_nico = User::find(2);
     $user_richard = User::find(3);
+    $user_larry = User::find(4);
+    $user_franz = User::find(5);
+    $user_logan = User::find(6);
+    $user_steve = User::find(7);
+    $user_bill = User::find(8);
+    $user_mike = User::find(9);
 
     $project_james = Project::find(1);
     $project_nico = Project::find(2);
     $project_richard = Project::find(3);
 
     $skill_prog = GeneralSkill::find(1);
+    $skill_gameengine = GeneralSkill::find(2);
+    $skill_web = GeneralSkill::find(3);
     $skill_art_2d = GeneralSkill::find(4);
+    $skill_art_3d = GeneralSkill::find(5);
+    $skill_music = GeneralSkill::find(6);
+    $skill_marketing = GeneralSkill::find(7);
 
     ProjectCollaborator::create([
       'skill_id' => $skill_prog->id,
@@ -51,6 +62,60 @@ class CollaboratorTableSeeder extends Seeder
       'from_collaborator' => true,
       'invite_message' => 'initial seed',
     ]);
+    ProjectCollaborator::create([
+      'skill_id' => $skill_gameengine->id,
+      'project_id' => $project_nico->id,
+      'is_project_owner' => false,
+      'user_id' => $user_larry->id,
+      'accepted' => true,
+      'from_collaborator' => true,
+      'invite_message' => 'initial seed',
+    ]);
+    ProjectCollaborator::create([
+      'skill_id' => $skill_web->id,
+      'project_id' => $project_nico->id,
+      'is_project_owner' => false,
+      'user_id' => $user_franz->id,
+      'accepted' => true,
+      'from_collaborator' => true,
+      'invite_message' => 'initial seed',
+    ]);
+    ProjectCollaborator::create([
+      'skill_id' => $skill_art_2d->id,
+      'project_id' => $project_nico->id,
+      'is_project_owner' => false,
+      'user_id' => $user_logan->id,
+      'accepted' => true,
+      'from_collaborator' => true,
+      'invite_message' => 'initial seed',
+    ]);
+    ProjectCollaborator::create([
+      'skill_id' => $skill_art_3d->id,
+      'project_id' => $project_nico->id,
+      'is_project_owner' => false,
+      'user_id' => $user_steve->id,
+      'accepted' => true,
+      'from_collaborator' => true,
+      'invite_message' => 'initial seed',
+    ]);
+    ProjectCollaborator::create([
+      'skill_id' => $skill_marketing->id,
+      'project_id' => $project_nico->id,
+      'is_project_owner' => false,
+      'user_id' => $user_bill->id,
+      'accepted' => true,
+      'from_collaborator' => true,
+      'invite_message' => 'initial seed',
+    ]);
+    ProjectCollaborator::create([
+      'skill_id' => $skill_music->id,
+      'project_id' => $project_nico->id,
+      'is_project_owner' => false,
+      'user_id' => $user_mike->id,
+      'accepted' => true,
+      'from_collaborator' => true,
+      'invite_message' => 'initial seed',
+    ]);
 
     ProjectCollaborator::create([
       'skill_id' => $skill_art_2d->id,
@@ -59,6 +124,16 @@ class CollaboratorTableSeeder extends Seeder
       'user_id' => $user_james->id,
       'accepted' => false,
       'from_collaborator' => false,
+      'invite_message' => 'initial seed',
+    ]);
+
+    ProjectCollaborator::create([
+      'skill_id' => $skill_prog->id,
+      'project_id' => $project_nico->id,
+      'is_project_owner' => false,
+      'user_id' => $user_richard->id,
+      'accepted' => true,
+      'from_collaborator' => true,
       'invite_message' => 'initial seed',
     ]);
   }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -9,8 +9,11 @@ class UsersTableSeeder extends Seeder
   {
     $skill_prog_id = 1;
     $skill_gameengine_id = 2;
+    $skill_web_id = 3;
     $skill_art_2d_id = 4;
     $skill_art_3d_id = 5;
+    $skill_music_id = 6;
+    $skill_marketing_id = 7;
     $lang_english_id = 1;
     $lang_french_id = 2;
     $lang_german_id = 2;
@@ -100,8 +103,163 @@ class UsersTableSeeder extends Seeder
       'image' => 'https://s-media-cache-ak0.pinimg.com/236x/bf/f5/d0/bff5d074d399bdfec6071e9168398406.jpg',
     ]);
 
+    DB::table('general_skill_user')->insert([
+      'general_skill_id' => $skill_prog_id,
+      'user_id' => $user->id,
+    ]);
+
     DB::table('language_user')->insert([
       'language_id' => $lang_french_id,
+      'user_id' => $user->id,
+    ]);
+
+    $user = User::create([
+      'name' => 'Larry',
+      'email' => 'larry@larry.com',
+      'first_name' => 'Larry',
+      'last_name' => 'Jensen',
+      'talent_description' => $talent_description,
+      'password' => bcrypt('test'),
+      'github' => 'http://example.com/my_github',
+      'website' => 'http://example.com/my_project',
+      'stack_overflow' => 'http://example.com/stack_overflow',
+      'image' => 'http://i.imgur.com/Tjkm1C0.jpg',
+    ]);
+
+    DB::table('general_skill_user')->insert([
+      'general_skill_id' => $skill_prog_id,
+      'user_id' => $user->id,
+    ]);
+
+    DB::table('language_user')->insert([
+      'language_id' => $lang_french_id,
+      'user_id' => $user->id,
+    ]);
+
+    $user = User::create([
+      'name' => 'Franz',
+      'email' => 'franz@franz.com',
+      'first_name' => 'Franz',
+      'last_name' => 'Goldstein',
+      'talent_description' => $talent_description,
+      'password' => bcrypt('test'),
+      'github' => 'http://example.com/my_github',
+      'website' => 'http://example.com/my_project',
+      'stack_overflow' => 'http://example.com/stack_overflow',
+      'image' => 'http://i.imgur.com/Pdfl0cO.png',
+    ]);
+
+    DB::table('general_skill_user')->insert([
+      'general_skill_id' => $skill_web_id,
+      'user_id' => $user->id,
+    ]);
+
+    DB::table('language_user')->insert([
+      'language_id' => $lang_english_id,
+      'user_id' => $user->id,
+    ]);
+
+    $user = User::create([
+      'name' => 'Logan',
+      'email' => 'logan@logan.com',
+      'first_name' => 'Logan',
+      'last_name' => 'Schmutz',
+      'talent_description' => $talent_description,
+      'password' => bcrypt('test'),
+      'github' => 'http://example.com/my_github',
+      'website' => 'http://example.com/my_project',
+      'stack_overflow' => 'http://example.com/stack_overflow',
+      'image' => 'http://i.imgur.com/dyBJMyY.jpg',
+    ]);
+
+    DB::table('general_skill_user')->insert([
+      'general_skill_id' => $skill_art_2d_id,
+      'user_id' => $user->id,
+    ]);
+
+    DB::table('language_user')->insert([
+      'language_id' => $lang_german_id,
+      'user_id' => $user->id,
+    ]);
+
+    $user = User::create([
+      'name' => 'Steve',
+      'email' => 'steve@steve.com',
+      'first_name' => 'Steve',
+      'last_name' => 'Vai',
+      'talent_description' => $talent_description,
+      'password' => bcrypt('test'),
+      'github' => 'http://example.com/my_github',
+      'website' => 'http://example.com/my_project',
+      'stack_overflow' => 'http://example.com/stack_overflow',
+      'image' => 'http://i.imgur.com/IPtPvxU.jpg?1',
+    ]);
+
+    DB::table('general_skill_user')->insert([
+      'general_skill_id' => $skill_art_3d_id,
+      'user_id' => $user->id,
+    ]);
+
+    DB::table('language_user')->insert([
+      'language_id' => $lang_french_id,
+      'user_id' => $user->id,
+    ]);
+    DB::table('language_user')->insert([
+      'language_id' => $lang_german_id,
+      'user_id' => $user->id,
+    ]);
+
+    $user = User::create([
+      'name' => 'Bill',
+      'email' => 'bill@bill.com',
+      'first_name' => 'Bill',
+      'last_name' => 'Hathaway',
+      'talent_description' => $talent_description,
+      'password' => bcrypt('test'),
+      'github' => 'http://example.com/my_github',
+      'website' => 'http://example.com/my_project',
+      'stack_overflow' => 'http://example.com/stack_overflow',
+      'image' => 'http://i.imgur.com/eFhCMYv.jpg',
+    ]);
+
+    DB::table('general_skill_user')->insert([
+      'general_skill_id' => $skill_music_id,
+      'user_id' => $user->id,
+    ]);
+
+    DB::table('language_user')->insert([
+      'language_id' => $lang_french_id,
+      'user_id' => $user->id,
+    ]);
+
+    $user = User::create([
+      'name' => 'Mike',
+      'email' => 'mike@mike.com',
+      'first_name' => 'Mike',
+      'last_name' => '',
+      'talent_description' => $talent_description,
+      'password' => bcrypt('test'),
+      'github' => 'http://example.com/my_github',
+      'website' => 'http://example.com/my_project',
+      'stack_overflow' => 'http://example.com/stack_overflow',
+      'image' => 'http://i.imgur.com/wDSw3m5.jpg',
+    ]);
+
+    DB::table('general_skill_user')->insert([
+      'general_skill_id' => $skill_marketing_id,
+      'user_id' => $user->id,
+    ]);
+
+    DB::table('language_user')->insert([
+      'language_id' => $lang_french_id,
+      'user_id' => $user->id,
+    ]);
+    DB::table('language_user')->insert([
+      'language_id' => $lang_english_id,
+      'user_id' => $user->id,
+    ]);
+    DB::table('language_user')->insert([
+      'language_id' => $lang_german_id,
       'user_id' => $user->id,
     ]);
   }
