56a320e07b2393cd8abb17f650488aae62eeb04a
Michael Caraccio <michael.caraccio@gmail.com>
2016-09-30 00:40:22 +0200

    Close #27 by adding My project and My profile to nav bar

--- resources/views/nav_bar.blade.php
+++ resources/views/nav_bar.blade.php
@@ -13,18 +13,21 @@
   </ul>
   <ul class="nav navbar-nav pull-xs-right">
     @if (Auth::guest())
-      <li class="nav-item"><a class="nav-link" href="{{ url('/login') }}">Login</a></li>
-      <li class="nav-item"><a class="nav-link" href="{{ url('/register') }}">Register</a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ url('/login') }}">Login</a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ url('/register') }}">Register</a></li>
     @else
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-          {{ Auth::user()->name }} <span class="caret"></span>
-        </a>
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+        {{ Auth::user()->name }} <span class="caret"></span>
+      </a>
 
-        <ul class="dropdown-menu" role="menu">
-          <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-        </ul>
-      </li>
+      <ul class="dropdown-menu" role="menu">
+        <li><a href="{{ url('/talents/'.Auth::user()->id) }}"><i class="fa fa-btn fa-user"></i> My profile</a></li>
+        <li><a href="{{ url('/talents/'.Auth::user()->id.'/projects') }}"><i class="fa fa-btn fa-cogs"></i> My projects</a></li>
+        <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i> Logout</a></li>
+      </ul>
+
+    </li>
     @endif
   </ul>
 </nav>
---
+++ resources/views/users/project.blade.php
@@ -0,0 +1,38 @@
+@extends('layout')
+
+@section('content')
+
+
+  @if($user->collaborations)
+    <div class="row">
+      <label for="col-md-12"><strong>Projects</strong></label>
+      <div class="col-md-12">
+        @foreach ($user->collaborations as $collaboration)
+          <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12">
+            <div class="media card card-outline-primary ">
+              <a class="media-left" href="{{$collaboration->project->path()}}">
+                <img class="media-object image-64"
+                  @if($collaboration->project->image)
+                    src="{{$collaboration->project->image}}"
+                  @else
+                    src="{{asset('assets/images/default_project.png')}}"
+                  @endif
+                  alt="Project Image {{$collaboration->project->title}}">
+              </a>
+              <div class="media-body">
+                <a href="{{$collaboration->project->path()}}">
+                  <h4 class="media-heading">{{$collaboration->project->title}}</h4>
+                </a>
+                <span class="tag tag-primary">{{$collaboration->skill->name}}</span>
+                @if($collaboration->is_project_owner)
+                  <span class="tag tag-pill tag-danger">Owner</span>
+                @endif
+              </div>
+            </div>
+          </div>
+        @endforeach
+      </div>
+    </div>
+  @endif
+
+@endsection
