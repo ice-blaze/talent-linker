c25c30f3009d9493eb3bf97c894425990a27390b
ice-blaze <franketip@gmail.com>
2016-10-02 06:20:18 +0200

    Add defaults values on lat/lng/search_distance

--- database/migrations/2016_09_24_141724_user_geolocalisation.php
+++ database/migrations/2016_09_24_141724_user_geolocalisation.php
@@ -7,9 +7,9 @@ class UserGeolocalisation extends Migration
 {
     public function up() {
       Schema::table('users', function (Blueprint $table) {
-        $table->double('lat');
-        $table->double('lng');
-        $table->float('find_distance');
+        $table->double('lat')->default(0);
+        $table->double('lng')->default(0);
+        $table->float('find_distance')->default(1);
       });
     }
 
