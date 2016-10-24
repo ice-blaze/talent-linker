3a3e3d4eb3850143715f0e9cda24aa18097870d5
Ice-Blaze <franketip@gmail.com>
2016-10-24 15:08:48 +0200

    Add new test files
    test

---
+++ tests/create_project_test.php
@@ -0,0 +1,9 @@
+<?php
+
+class create_project_test extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/create_user_test.php
@@ -0,0 +1,22 @@
+<?php
+
+class create_user_test extends TestCase
+{
+    public function testDoesLoginPagesExist()
+    {
+        $this->assertTrue(true);
+        
+        // $this->visit('/');
+        // $this->press('Login');
+        // $this->visit('/');
+        // $this->press('Register');
+    }
+    
+    public function testRegister()
+    {
+        $this->assertTrue(true);
+        
+        // $this->visit('/register');
+        // $this->type('Test Test', 'name123');
+    }
+}
