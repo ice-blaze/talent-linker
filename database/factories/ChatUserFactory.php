9c3deaa2ee3f64e30aa80d571ee3c466414ef084
Ice-Blaze <franketip@gmail.com>
2016-10-31 09:23:22 +0100

    Continue tests
    Not finished but wanted to work on my laptop

--- database/factories/ChatUserFactory.php
+++ database/factories/ChatUserFactory.php
@@ -1,10 +1,8 @@
 <?php
 
-// $factory->define(App\User::class, function (Faker\Generator $faker) {
-//     return [
-//         'name' => $faker->name,
-//         'email' => $faker->safeEmail,
-//         'password' => bcrypt(str_random(10)),
-//         'remember_token' => str_random(10),
-//     ];
-// });
+$factory->define(App\ChatUser::class, function (Faker\Generator $faker) {
+    return [
+        'content' => $faker->text,
+        'seen' => false,
+    ];
+});
