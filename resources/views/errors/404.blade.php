d0333029d277f139b9e6533cac54d83af07385d6
Michael Caraccio <michael.caraccio@gmail.com>
2016-11-03 23:40:18 +0100

    Fix 404 error, create new layout for error pages and move them to layout folder

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,8 +1,16 @@
-@extends('layout')
+@extends('layouts.layout-error')
 
 @section('content')
-  <div class="row">
-    Oops... 404 Error...
-    <a class="btn btn-primary" href="/">Go Back</a>
-  </div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 col-centered">
+			<div class="row">
+				Oops... 404 Error...
+			</div>
+			<div class="row">
+				<a class="btn btn-primary" href="/">Go Back</a>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
