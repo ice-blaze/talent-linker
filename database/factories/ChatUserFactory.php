483af5833bc4e6042cc63e8a1550da77a5efb897
Ice-Blaze <franketip@gmail.com>
2016-11-05 12:43:11 +0100

    Add the chat user factory

--- database/factories/ChatUserFactory.php
+++ database/factories/ChatUserFactory.php
@@ -3,6 +3,32 @@
 $factory->define(App\ChatUser::class, function (Faker\Generator $faker) {
     return [
         'content' => $faker->text,
+        'reciever_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'sender_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
         'seen' => false,
     ];
 });
+
+$factory->defineAs(App\ChatUser::class, 'no_users', function (Faker\Generator $faker) {
+    return [
+        'content' => $faker->text,
+        'seen' => false,
+    ];
+});
+
+// Only for laravel 5.3
+// $factory->state(App\ChatUser::class, 'seen', function ($faker) {
+//     return [
+//         'seen' => true,
+//     ];
+// });
+
+// $factory->state(App\ChatUser::class, 'unseen', function ($faker) {
+//     return [
+//         'seen' => false,
+//     ];
+// });
--- tests/visitor_test.php
+++ tests/visitor_test.php
@@ -120,4 +120,8 @@ class visitor_test extends TestCase
     // $feedback = factory(App\Feedback::class)
     //     ->create();
     // }
+    // $chat = factory(App\ChatUser::class, 'no_users')->make();
+    // $chat->sender()->associate($user1);
+    // $chat->reciever()->associate($user2);
+    // $chat->create();
 }
