1ce395f9dfbe26df621d97c44732ecc322ca602e
Ice-Blaze <franketip@gmail.com>
2016-11-06 12:09:19 +0100

    Start to look on how to do it

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -3,5 +3,10 @@
 $factory->define(App\Project::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
+        'short_description' => $faker->text,
+        'long_description' => $faker->text,
+        'github_link' => $faker->link,
+        'siteweb_link' => $faker->link,
+        // 'post_id' => App\Post::all()->random()->id,
     ];
 });
