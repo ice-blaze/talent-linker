a8209ff207bb81c5b5c5740ba8123b04ef2f02a4
ice-blaze <franketip@gmail.com>
2016-11-07 10:17:13 +0100

    Add project comment factory

--- database/factories/ProjectCommentFactory.php
+++ database/factories/ProjectCommentFactory.php
@@ -1,10 +1,37 @@
 <?php
 
-// $factory->define(App\User::class, function (Faker\Generator $faker) {
-//     return [
-//         'name' => $faker->name,
-//         'email' => $faker->safeEmail,
-//         'password' => bcrypt(str_random(10)),
-//         'remember_token' => str_random(10),
-//     ];
-// });
+$factory->define(App\ProjectComment::class, function (Faker\Generator $faker) {
+    return [
+        'content' => $faker->paragraph,
+        'private' => false,
+    ];
+});
+
+$factory->state(App\ProjectComment::class, 'with_user', function ($faker) {
+    return [
+        'user_id' => function(){
+            return factory(App\User::class)->create()->id;
+        },
+    ];
+});
+
+$factory->state(App\ProjectComment::class, 'with_project', function ($faker) {
+    return [
+        'project_id' => function(){
+            return factory(App\Project::class)->create()->id;
+        },
+    ];
+});
+
+$factory->state(App\ProjectComment::class, 'private', function ($faker) {
+    return [
+        'private' => true,
+    ];
+});
+
+$factory->state(App\ProjectComment::class, 'public', function ($faker) {
+    return [
+        'private' => false,
+    ];
+});
+
