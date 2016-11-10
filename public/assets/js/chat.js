321f6775c46ee879813e3ff4f73ac7bdbcaf5653
Michael Caraccio <michael.caraccio@gmail.com>
2016-11-10 22:45:27 +0100

    Change tabs in spaces

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -1,18 +1,17 @@
 // Wait onload - jQuery load and then we use it
 window.onload = function () {
-	
-	// ----------------------------------------------------------------------------
-	// Check message input. Disable send button if text is empty and enable it text 
-	// it not empty
-	// ----------------------------------------------------------------------------
 
-	$('#chats-index-user-message').keyup(function(){
-		if($(this).val() === ""){
-			$('#chats-index-message-send').prop("disabled",true);
-		}
-		else{
-			$('#chats-index-message-send').prop("disabled",false);
-		}
-	});
+    // ----------------------------------------------------------------------------
+    // Check message input. Disable send button if text is empty and enable it text 
+    // it not empty
+    // ----------------------------------------------------------------------------
 
+    $('#chats-index-user-message').keyup(function(){
+        if($(this).val() === ""){
+            $('#chats-index-message-send').prop("disabled",true);
+        }
+        else{
+            $('#chats-index-message-send').prop("disabled",false);
+        }
+    });
 };
