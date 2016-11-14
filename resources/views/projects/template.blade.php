ffceecdceaf13d54969578800146d149a3d79734
Michael Caraccio <michael.caraccio@gmail.com>
2016-11-14 09:35:53 +0100

    Re-apply fix #106 after auto-merge

--- resources/views/projects/template.blade.php
+++ resources/views/projects/template.blade.php
@@ -38,90 +38,108 @@
             {{-- could be simplified I guess --}}
             {{ $description = old('long_description') }}
             @if (isset($project)) {{$description = $project->long_description}} @endif
-                @include('helpers/ckeditor', [
-                    'name' => "long_description",
-                    'content' => $description,
-                    'placeholder' => "Project Long Description",
-                ])
+            @include('helpers/ckeditor', [
+                'name' => "long_description",
+                'content' => $description,
+                'placeholder' => "Project Long Description",
+            ])
         </div>
-        @if ($errors->has('long_description'))
-            <span class="help-block form-control-feedback">
-                <strong>{{ $errors->first('long_description') }}</strong>
-            </span>
-        @endif
-    </div>
+            @if ($errors->has('long_description'))
+                <span class="help-block form-control-feedback">
+                    <strong>{{ $errors->first('long_description') }}</strong>
+                </span>
+            @endif
+        </div>
 
-    <div class="form-group{{ $errors->has('image') ? ' has-danger' : '' }}">
-        <label for="image">Image</label>
-        <input name="image" type="url" class="form-control{{ $errors->has('image') ? ' form-control-danger' : '' }}" id="image"
-        placeholder="Image URL" value="{{ $project->image or old('image') }}">
+        <div class="form-group{{ $errors->has('image') ? ' has-danger' : '' }}">
+            <label for="image">Image</label>
+            <input name="image" type="url" class="form-control{{ $errors->has('image') ? ' form-control-danger' : '' }}" id="image"
+            placeholder="Image URL" value="{{ $project->image or old('image') }}">
 
-        @if ($errors->has('image'))
-            <span class="help-block form-control-feedback">
-                <strong>{{ $errors->first('image') }}</strong>
-            </span>
-        @endif
-    </div>
+            @if ($errors->has('image'))
+                <span class="help-block form-control-feedback">
+                    <strong>{{ $errors->first('image') }}</strong>
+                </span>
+            @endif
+        </div>
 
-    <div class="form-group{{ $errors->has('github_link') ? ' has-danger' : '' }}">
-        <label for="github_link">GithHub</label>
-        <input name="github_link" type="url" class="form-control{{ $errors->has('github_link') ? ' form-control-danger' : '' }}" id="github_link"
-        placeholder="GitHub Project URL" value="{{ $project->github_link or old('github_link') }}">
+        <div class="form-group{{ $errors->has('github_link') ? ' has-danger' : '' }}">
+            <label for="github_link">GithHub</label>
+            <input name="github_link" type="url" class="form-control{{ $errors->has('github_link') ? ' form-control-danger' : '' }}" id="github_link"
+            placeholder="GitHub Project URL" value="{{ $project->github_link or old('github_link') }}">
 
-        @if ($errors->has('github_link'))
-            <span class="help-block form-control-feedback">
-                <strong>{{ $errors->first('github_link') }}</strong>
-            </span>
-        @endif
-    </div>
+            @if ($errors->has('github_link'))
+                <span class="help-block form-control-feedback">
+                    <strong>{{ $errors->first('github_link') }}</strong>
+                </span>
+            @endif
+        </div>
 
-    <div class="form-group{{ $errors->has('website_link') ? ' has-danger' : '' }}">
-        <label for="website_link">Website</label>
-        <input name="website_link" type="url" class="form-control{{ $errors->has('website_link') ? ' form-control-danger' : '' }}" id="website_link"
-        placeholder="Stack Overflow URL" value="{{ $project->website_link or old('website_link') }}">
+        <div class="form-group{{ $errors->has('website_link') ? ' has-danger' : '' }}">
+            <label for="website_link">Website</label>
+            <input name="website_link" type="url" class="form-control{{ $errors->has('website_link') ? ' form-control-danger' : '' }}" id="website_link"
+            placeholder="Stack Overflow URL" value="{{ $project->website_link or old('website_link') }}">
 
-        @if ($errors->has('website_link'))
-            <span class="help-block form-control-feedback">
-                <strong>{{ $errors->first('website_link') }}</strong>
-            </span>
-        @endif
-    </div>
+            @if ($errors->has('website_link'))
+                <span class="help-block form-control-feedback">
+                    <strong>{{ $errors->first('website_link') }}</strong>
+                </span>
+            @endif
+        </div>
 
-    @yield('collaborators')
+        @yield('collaborators')
 
-    <div class="form-group{{ $errors->has('general_skills') ? ' has-danger' : '' }}">
-        <label for="general_skills">Skills</label>
-        <ul name="general_skills[]" id="general_skills" >
+        <div class="form-group{{ $errors->has('general_skills') ? ' has-danger' : '' }}">
+            <label for="general_skills">Skills</label>
+            <ul name="general_skills[]" id="general_skills" >
 
-            @foreach($general_skills as $option)
-            <li>
-                {{$option->name}}
-                <input type="number" name="general_skills[{{ $option->id }}]" value="{{ (collect(old('general_skills'))->count() > 0) ? intval(collect(old('general_skills'))->toArray()[$option->id]):0}}" placeholder="0" min="0" step="1">
-            </li>
-            @endforeach
+                <?php
+                $skills_array = collect(array_pluck($general_skills->toArray(), 'pivot.count', 'id'));
+                ?>
 
-        </ul>
-    </div>
+                @foreach(App\GeneralSkill::all() as $option)
+                @if (count(collect(old('general_skills'))) > 0)
+                    <li>
+                        {{$option->name}}
+                        <input type="number" name="general_skills[{{ $option->id }}]" value="{{ (collect(old('general_skills'))->count() > 0) ? intval(collect(old('general_skills'))->toArray()[$option->id]):0}}" placeholder="0" min="0" step="1">
+                    </li>
+                @else
+                    <li>
 
-    <div class="form-group{{ $errors->has('languages') ? ' has-danger' : '' }}">
-        <label for="languages">Languages</label>
-        <select name="languages[]" id="languages" class="selectpicker {{ $errors->has('name') ? ' form-control-danger' : '' }}" multiple>
-            @foreach($languages as $option)
-                <option value="{{ $option->id }}" {{ (collect(old('languages'))->contains($option->id)) ? 'selected':'' }}>{{ $option->name }}</option>
-            @endforeach
-        </select>
-        @if ($errors->has('languages'))
-            <br>
-            <span class="help-block form-control-feedback">
-                <strong>{{ $errors->first('languages') }}</strong>
-            </span>
-        @endif
-    </div>
+                        {{$option->name}}
+                        <input type="number" name="general_skills[{{ $option->id }}]" value="{{ ($skills_array->keys()->contains($option->id)) ? intval($skills_array[$option->id]):0}}" placeholder="0" min="0" step="1">
+                    </li>
+                @endif
+                @endforeach
 
-    <div class="form-group">
-        <button type="submit" class="btn btn-primary" name="submit_project">
+            </ul>
+        </div>
+        <div class="form-group{{ $errors->has('languages') ? ' has-danger' : '' }}">
+            <label for="languages">Languages</label>
+            <select name="languages[]" id="languages" class="selectpicker {{ $errors->has('name') ? ' form-control-danger' : '' }}" multiple>
+
+                @foreach(App\Language::all() as $option)
+                    @if (count(collect(old('languages'))) > 0)
+                        <option value="{{ $option->id }}" {{ (collect(old('languages'))->contains($option->id)) ? 'selected':'' }}>{{ $option->name }}</option>
+                    @else
+                        <option value="{{ $option->id }}" {{ ($languages->contains($option->id)) ? 'selected':'' }}>{{ $option->name }}</option>
+                    @endif
+                @endforeach
+
+            </select>
+            @if ($errors->has('languages'))
+                <br>
+                <span class="help-block form-control-feedback">
+                    <strong>{{ $errors->first('languages') }}</strong>
+                </span>
+            @endif
+        </div>
+
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary" name="submit_project">
             @yield('button_name')
         </button>
+
     </div>
 </form>
 @stop
