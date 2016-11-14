59b1f20028b3f4a1bd52d9f416ca49215e99c48c
Ice-Blaze <franketip@gmail.com>
2016-11-14 01:11:45 +0100

    Fix blade indentation
    2 spaces to 4 spaces

--- resources/views/invitations/index_project.blade.php
+++ resources/views/invitations/index_project.blade.php
@@ -1,50 +1,49 @@
 @extends('layouts.layout')
 
 @section('content')
-  <div class="row">
-    <h1>Invitation on project: <a href="{{$project->path()}}">{{$project->name}}</a></h1>
-  </div>
+    <div class="row">
+        <h1>Invitation on project: <a href="{{$project->path()}}">{{$project->name}}</a></h1>
+    </div>
 
-  <div class="row">
-    @forelse($pendings as $collaborator)
-      <li class="list-group-item">
-        <a href="{{$collaborator->user->path()}}">{{$collaborator->user->name}}</a>
-        - skill: {{$collaborator->skill->name}}
-        - invited the {{$collaborator->created_at}}
-        @if($collaborator->accepted)
-          - accepted {{$collaborator->accepted_date}}
-        @else
-          - pending ...
-        @endif
-        @if($project->owner->user->id == Auth::user()->id)
-          @if( $collaborator->from_collaborator && !$collaborator->accepted )
-            <form method="post" action="/invitations/{{ $project->id }}/{{ $collaborator->user->id }}/accept">
-              {{ method_field('patch') }}
-              {{ csrf_field() }}
-              <div class="form-group text-right">
-                <button type="submit" class="btn btn-primary" name="accept{{ $collaborator->id }}">Accept</button>
-              </div>
-            </form>
-          @endif
-          <form method="post" action="/invitations/{{ $project->id }}/{{ $collaborator->user_id }}/{{ $collaborator->id }}">
-            {{ method_field('delete') }}
-            {{ csrf_field() }}
-            <div class="form-group text-right">
-              <button type="submit" class="btn btn-danger" name="delete{{ $collaborator->id }}">
+    <div class="row">
+        @forelse($pendings as $collaborator)
+            <li class="list-group-item">
+                <a href="{{$collaborator->user->path()}}">{{$collaborator->user->name}}</a>
+                - skill: {{$collaborator->skill->name}}
+                - invited the {{$collaborator->created_at}}
                 @if($collaborator->accepted)
-                  Kick from project
+                    - accepted {{$collaborator->accepted_date}}
                 @else
-                  Delete Request
+                    - pending ...
                 @endif
-              </button>
-            </div>
-          </form>
-        @endif
-
-      </li>
-    @empty
-      <br>
-      <span class="lead col-centered">No invitations...</span>
-    @endforelse
-  </div>
+                @if($project->owner->user->id == Auth::user()->id)
+                    @if( $collaborator->from_collaborator && !$collaborator->accepted )
+                        <form method="post" action="/invitations/{{ $project->id }}/{{ $collaborator->user->id }}/accept">
+                            {{ method_field('patch') }}
+                            {{ csrf_field() }}
+                            <div class="form-group text-right">
+                                <button type="submit" class="btn btn-primary" name="accept{{ $collaborator->id }}">Accept</button>
+                            </div>
+                        </form>
+                    @endif
+                    <form method="post" action="/invitations/{{ $project->id }}/{{ $collaborator->user_id }}/{{ $collaborator->id }}">
+                        {{ method_field('delete') }}
+                        {{ csrf_field() }}
+                        <div class="form-group text-right">
+                            <button type="submit" class="btn btn-danger" name="delete{{ $collaborator->id }}">
+                                @if($collaborator->accepted)
+                                    Kick from project
+                                @else
+                                    Delete Request
+                                @endif
+                            </button>
+                        </div>
+                    </form>
+                @endif
+            </li>
+        @empty
+            <br>
+            <span class="lead col-centered">No invitations...</span>
+        @endforelse
+    </div>
 @endsection
