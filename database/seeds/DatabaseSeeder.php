c73e3a6c176b4a47e39cc9b43dcb5f72141ca2ae
Ice-Blaze <franketip@gmail.com>
2016-11-15 22:52:24 +0100

    Add chat user seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(ProjectsTableSeeder::class);
         $this->call(CollaboratorTableSeeder::class);
+        $this->call(ChatUsersTableSeeder::class);
     }
 }
