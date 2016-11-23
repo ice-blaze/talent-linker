34cfb3ba50094892ba178800f7dafec1d2698506
Michael Caraccio <michael.caraccio@gmail.com>
2016-11-23 14:40:06 +0100

    Uncomment admin middleware

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Auth;
 class Admin
 {
     // TODO Will be available at #176.
-    /*public function handle($request, Closure $next)
+    public function handle($request, Closure $next)
     {
         if (Auth::check() && Auth::user()->is_admin) {
             return $next($request);
         }
         return redirect('/');
-    }*/
+    }
 }
