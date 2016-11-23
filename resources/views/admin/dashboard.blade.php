3f478eaf50580ed611744243b72921e38a76da14
Michael Caraccio <michael.caraccio@gmail.com>
2016-11-23 13:33:21 +0100

    Change UI for Dashboard blade

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,64 +4,48 @@
 <div class="row">
     <div class="col-md-12">
         <div class="panel panel-default">
-            <div class="panel-heading">Dashboard</div>
+            <h1>Dashboard</h1>
+            <hr>
+            <div class="card-deck-wrapper">
+                <div class="card-deck">
 
-            <div class="panel-body">
-                <div>
-                    <p>Welcome {{{Auth::user()->name}}}</p>
+                    <div class="card">
+                        <div class="card-header">
+                            Users
+                        </div>
+                        <div class="card-block">
+                            {{ App\User::count() }} Users
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            Projects
+                        </div>
+                        <div class="card-block">
+                            {{ App\Project::count() }} Projects <br>
+                            {{ App\ProjectComment::count() }} Comments
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            Feedbacks
+                        </div>
+                        <div class="card-block">
+                            {{ App\Feedback::count() }} Feedbacks
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            Chat
+                        </div>
+                        <div class="card-block">
+                            {{ App\ChatUser::count() }} User chat messages
+                        </div>
+                    </div>
                 </div>
-
-                <hr>
-
-                <div class="row">
-                    <div class="col-md-2">
-                        <div class="card-container">
-                            <div class="card-left">
-                                <i class="fa fa-users" style="color:white;" aria-hidden="true"></i>
-                            </div>
-                            <div class="card-right">
-                                {{ App\User::count() }} Users
-                            </div>
-                        </div>
-                    </div>
-
-                    
-
-                    <div class="col-md-2">
-                        <div class="card-container">
-                            <div class="card-left">
-                                <i class="fa fa-paint-brush" style="color:white;" aria-hidden="true"></i>
-                            </div>
-                            <div class="card-right">
-                                {{ App\Project::count() }} Projects <br>
-                                {{ App\ProjectComment::count() }} Comments
-                            </div>
-                        </div>
-                    </div>
-
-                     <div class="col-md-3">
-                        <div class="card-container">
-                            <div class="card-left">
-                                <i class="fa fa-paint-brush" style="color:white;" aria-hidden="true"></i>
-                            </div>
-                            <div class="card-right">
-                                {{ App\Feedback::count() }} Feedbacks
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-2">
-                        <div class="card-container">
-                            <div class="card-left">
-                                <i class="fa fa-paint-brush" style="color:white;" aria-hidden="true"></i>
-                            </div>
-                            <div class="card-right">
-                                {{ App\ChatUser::count() }} User chat messages
-                            </div>
-                        </div>
-                    </div>  
-                </div>
-                <hr>
             </div>
         </div>
     </div>
