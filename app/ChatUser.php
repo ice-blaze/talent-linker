d22a3ef36c09f2b0e45ee8aad17abf7a8c4c5d19
Déruaz Vincent <deruazvincent@gmail.com>
2016-11-24 15:43:34 +0100

    Update ChatUser.php
    fix StyleCI #3

--- app/ChatUser.php
+++ app/ChatUser.php
@@ -9,6 +9,7 @@ class ChatUser extends Model
 {
     protected $fillable = ['content'];
     
+    
     public function reciever()
     {
         return $this->belongsTo(User::class, 'reciever_id');
