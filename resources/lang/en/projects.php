37f6eaa840a6297acbda655862e7eb8fd9a2ef09
Michael Caraccio <michael.caraccio@gmail.com>
2017-07-31 00:45:58 +0200

    projects localization done

---
+++ resources/lang/en/projects.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    'create_project' => 'Create project',
+    'your_skill_as_project_owner' => 'Your skill as project owner',
+    'collaborators' => 'Collaborators',
+    'can_only_delete_collaborators' => 'can only delete collaborators',
+    'edit_project' => 'Edit project',
+    'create_project' => 'Create project',
+    'near_you' => 'Near you',
+    'not_near' => 'Not near',
+    'last_update' => 'Last update',
+    'no_projects' => 'No projects',
+    'see_pendings' => '',
+    'private_chat' => '',
+    'delete' => '',
+    'invitation_is_pending' => '',
+    'join_the_project' => '',
+    'owner' => '',
+    'languages' => '',
+    'place' => '',
+    'website' => '',
+    'comments' => '',
+    'no_comments' => '',
+    'comment' => '',
+    'add_a_comment' => '',
+    'title' => '',
+    'short_description' => '',
+    'long_description' => '',
+    'image' => '',
+    'skills' => ''
+
+];
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,100 +2,100 @@
 
 @section('content')
 
-    @if(Auth::user())
-        <div class="row">
-            <div class="col-md-12 col-centered">
-                <a class="btn btn-primary" href="projects/create">Create Project</a>
-            </div>
-        </div>
-        <br>
-    @endif
+@if(Auth::user())
+<div class="row">
+  <div class="col-md-12 col-centered">
+    <a class="btn btn-primary" href="projects/create">{{ Trans('projects.create_project') }}</a>
+  </div>
+</div>
+<br>
+@endif
 
-    <div class="row">
-        <div class="col-md-8 offset-md-2 col-sm-12">
-            <form method="post" action="/projects">
-                {{ csrf_field() }}
-                <div class="form-group input-group">
-                    <a onclick="$(this).closest('form').submit()" class="search-button input-group-addon">
-                        <i class="fa fa-search" aria-hidden="true"></i>
-                    </a>
-                    <input name="search" placeholder="Search Project" class="form-control" value="{{ old('search') }}"/>
-                </div>
-                <div class="form-group">
-                    @if (Auth::user())
-                        @include('helpers/form_checkbox', [
-                            'name' => "near_by",
-                            'display' => '<i class="fa fa-map-marker" aria-hidden="true"></i> Near By',
-                            'id' => "near_by",
-                            'skill' => false,
-                        ])
-                    @endif
-                    @foreach($general_skills as $skill)
-                        @include('helpers/form_checkbox', [
-                            'name' => $skill->technical_name,
-                            'display' => $skill->name,
-                            'id' => $skill->id,
-                            'skill' => true,
-                        ])
-                    @endforeach
-                </div>
-                <button type="submit" class="hidden" name="search_button"/> {{-- tests can activate this form --}}
-            </form>
-        </div>
+<div class="row">
+  <div class="col-md-8 offset-md-2 col-sm-12">
+    <form method="post" action="/projects">
+      {{ csrf_field() }}
+      <div class="form-group input-group">
+        <a onclick="$(this).closest('form').submit()" class="search-button input-group-addon">
+          <i class="fa fa-search" aria-hidden="true"></i>
+        </a>
+        <input name="search" placeholder="Search Project" class="form-control" value="{{ old('search') }}"/>
+      </div>
+      <div class="form-group">
+        @if (Auth::user())
+        @include('helpers/form_checkbox', [
+          'name' => "near_by",
+          'display' => '<i class="fa fa-map-marker" aria-hidden="true"></i> Near By',
+          'id' => "near_by",
+          'skill' => false,
+          ])
+          @endif
+          @foreach($general_skills as $skill)
+          @include('helpers/form_checkbox', [
+            'name' => $skill->technical_name,
+            'display' => $skill->name,
+            'id' => $skill->id,
+            'skill' => true,
+            ])
+            @endforeach
+          </div>
+          <button type="submit" class="hidden" name="search_button"/> {{-- tests can activate this form --}}
+        </form>
+      </div>
     </div>
 
     <div class="card-deck-wrapper">
-        @forelse(array_chunk($projects->all(), 3) as $threeProjects)
-            <div class="card-deck">
-                @foreach($threeProjects as $project)
-                    <div class="card">
-                          <a href="{{ $project->path() }}">
-                              <img class="card-img-top img-fluid m-x-auto"
-                                  @if($project->image)
-                                      src="{{$project->image}}"
-                                  @else
-                                      src="{{asset('assets/images/default_project.png')}}"
-                                  @endif
-                                  alt="Project image">
-                          </a>
-                      <div class="card-block">
-                          <h4 class="card-title"><a href="{{ $project->path() }}">{{ $project->name }}</a></h4>
-                          @if (Auth::user())
-                              @if($project->isInSearchDistance(Auth::user()))
-                                  <span class="tag tag-pill tag-primary"><i class="fa fa-map-marker" aria-hidden="true"></i> Near You</span>
-                              @else
-                                  <span class="tag tag-pill tag-danger"><i class="fa fa-map-marker" aria-hidden="true"></i> Not Near</span>
-                              @endif
-                          @endif
-                      </div>
-                      {{-- <img class="card-img-top" src="..." alt="Card image cap"> --}}
-                      <div class="card-block">
-                          <p class="card-text">{{ $project->short_description }}</p>
-                          <p>
-                              @foreach($project->currentSkillAndWanted() as $skill)
-                                  <span
-                                      @if($skill['wanted'] > 0 && $skill['have'] >= $skill['wanted'])
-                                          class="tag tag-primary"
-                                      @else
-                                          <span class="tag tag-default"
-                                      @endif
-                                  >
-                                      {{$skill['skill']->name}} {{$skill['have']}} / {{$skill['wanted']}}
-                                  </span>
-                              @endforeach
-                              <br>
-                          </p>
-                          <p class="card-text"><small class="text-muted">Last updated {{$project->updated_at->diffForHumans()}}</small></p>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        @empty
-            <div class="row">
-                <div class="col-md-12 col-centered">
-                    <h2>No Projects...</h2>
-                </div>
-            </div>
-        @endforelse
+      @forelse(array_chunk($projects->all(), 3) as $threeProjects)
+      <div class="card-deck">
+        @foreach($threeProjects as $project)
+        <div class="card">
+          <a href="{{ $project->path() }}">
+            <img class="card-img-top img-fluid m-x-auto"
+            @if($project->image)
+            src="{{$project->image}}"
+            @else
+            src="{{asset('assets/images/default_project.png')}}"
+            @endif
+            alt="Project image">
+          </a>
+          <div class="card-block">
+            <h4 class="card-title"><a href="{{ $project->path() }}">{{ $project->name }}</a></h4>
+            @if (Auth::user())
+            @if($project->isInSearchDistance(Auth::user()))
+            <span class="tag tag-pill tag-primary"><i class="fa fa-map-marker" aria-hidden="true"></i> {{ Trans('projects.near_you') }}</span>
+            @else
+            <span class="tag tag-pill tag-danger"><i class="fa fa-map-marker" aria-hidden="true"></i> {{ Trans('projects.not_near') }}</span>
+            @endif
+            @endif
+          </div>
+          {{-- <img class="card-img-top" src="..." alt="Card image cap"> --}}
+          <div class="card-block">
+            <p class="card-text">{{ $project->short_description }}</p>
+            <p>
+              @foreach($project->currentSkillAndWanted() as $skill)
+              <span
+              @if($skill['wanted'] > 0 && $skill['have'] >= $skill['wanted'])
+              class="tag tag-primary"
+              @else
+              <span class="tag tag-default"
+              @endif
+              >
+              {{$skill['skill']->name}} {{$skill['have']}} / {{$skill['wanted']}}
+            </span>
+            @endforeach
+            <br>
+          </p>
+          <p class="card-text"><small class="text-muted">{{ Trans('projects.last_update') }} {{$project->updated_at->diffForHumans()}}</small></p>
+        </div>
+      </div>
+      @endforeach
     </div>
-@stop
+    @empty
+    <div class="row">
+      <div class="col-md-12 col-centered">
+        <h2>{{ Trans('projects.no_projects') }}...</h2>
+      </div>
+    </div>
+    @endforelse
+  </div>
+  @stop
