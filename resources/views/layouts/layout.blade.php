af19d73dfda10683e672bba62ec81ec09e6c04b1
Michael Caraccio <michael.caraccio@gmail.com>
2017-07-31 11:42:33 +0200

    global localization done

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Talent Linker</title>
+        <title>{{ Trans('global.website_name') }}</title>
 
         <!-- Latest compiled and minified CSS -->
         {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
