49fefe5d3808c1fda27edce7f1bf4de9118077f3
Michael Caraccio <michael.caraccio@gmail.com>
2017-07-31 11:38:23 +0200

    view localization done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,24 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="jumbotron">
-                <h1 class="display-3">Create some projects !</h1>
-                <p class="lead">
-                  Welcome on XXXX, a web site that helps people to find other talents in the world or in their region for
-                  audio-visual-multimedia projects.
-                </p>
-                <hr class="m-y-2">
-                <p class="lead">You have two choices</p>
-                <div class="row">
-                    <div class="col-xs-6 col-centered">
-                        <a class="btn btn-primary btn-lg" href="/projects" role="button">Find projects</a>
-                    </div>
-                    <div class="col-xs-6 col-centered">
-                        <a class="btn btn-primary btn-lg" href="/talents" role="button">Find talents</a>
-                    </div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="jumbotron">
+            <h1 class="display-3">{{ Trans('views.create_some_projects') }} !</h1>
+            <p class="lead">
+                {{ Trans('views.index_welcome_message') }}
+            </p>
+            <hr class="m-y-2">
+            <p class="lead">{{ Trans('views.you_have_two_choices') }}</p>
+            <div class="row">
+                <div class="col-xs-6 col-centered">
+                    <a class="btn btn-primary btn-lg" href="/projects" role="button">{{ Trans('views.find_projects') }}</a>
                 </div>
-                </p>
+                <div class="col-xs-6 col-centered">
+                    <a class="btn btn-primary btn-lg" href="/talents" role="button">{{ Trans('views.find_talents') }}</a>
+                </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
